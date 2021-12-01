90545df2d95a7f8ac553c7f6d9dae58cb044d111
Llamexis <polaczek0907@gmail.com>
2021-12-01 13:45:00 +0100

    Added first quest of day 1

---
+++ day 1/quest 1.py
@@ -0,0 +1,11 @@
+from sys import *
+previous_value = 0
+dane = []
+increased = 0
+with open("advent-of-code-2021\day 1\dane.txt", "r") as file:
+    for line in file:
+        dane.append(int(line))
+for value in range(len(dane)-1):
+    if dane[value] < dane[value + 1]:
+        increased+= 1
+print(increased)
