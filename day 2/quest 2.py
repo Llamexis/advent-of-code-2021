76bae6be2b40f1c5497556a682b9c99f964077c7
Llamexis <polaczek0907@gmail.com>
2021-12-02 18:38:34 +0100

    solved second part of day 2

--- day 2/quest 2.py
+++ day 2/quest 2.py
@@ -2,15 +2,17 @@ from sys import *
 
 horizontal_pos = 0
 vertical_pos = 0
+aim = 0
 
 with open("advent-of-code-2021/day 2/data.txt", "r") as file:
     for line in file:
         #print(line)
         if line[0] == 'f':
             horizontal_pos += int(line[-2])
+            vertical_pos += aim * int(line[-2])
         elif line[0] == 'u':
-            vertical_pos-= int(line[-2])
+            aim -= int(line[-2])
         elif line[0] == 'd':
-            vertical_pos+= int(line[-2])
+            aim += int(line[-2])
 
 print("multiplied values (depth and horizontal)",horizontal_pos * vertical_pos)
